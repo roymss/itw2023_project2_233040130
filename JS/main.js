c78c0816cfe2924f622c4ca86fa25bcccc917c5f
roymss <145090061+roymss@users.noreply.github.com>
2023-12-20 14:21:59 +0700

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+function show() {
+    const sidebar = document.querySelector(".sideNavbar");
+    sidebar.style.left = 0;
+}
+
+function hide() {
+    const sidebar = document.querySelector(".sideNavbar");
+    sidebar.style.left = "-100%";
+}
